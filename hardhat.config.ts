7a60bd1a2a011bdf22e8fd2fe7b02bdaac572fb0
Sam Bauch <sbauch@gmail.com>
2021-07-23 20:49:13 -0400

    Initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,81 @@
+import 'dotenv/config';
+import {HardhatUserConfig} from 'hardhat/types';
+import 'hardhat-deploy';
+import 'hardhat-deploy-ethers';
+import 'hardhat-gas-reporter';
+import '@typechain/hardhat';
+import 'solidity-coverage';
+import {node_url, accounts} from './utils/network';
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.7.1',
+  },
+  namedAccounts: {
+    deployer: 0,
+    simpleERC20Beneficiary: 1,
+  },
+  networks: {
+    hardhat: {
+      // process.env.HARDHAT_FORK will specify the network that the fork is made from.
+      // this line ensure the use of the corresponding accounts
+      accounts: accounts(process.env.HARDHAT_FORK),
+      forking: process.env.HARDHAT_FORK
+        ? {
+            url: node_url(process.env.HARDHAT_FORK),
+            blockNumber: process.env.HARDHAT_FORK_NUMBER
+              ? parseInt(process.env.HARDHAT_FORK_NUMBER)
+              : undefined,
+          }
+        : undefined,
+    },
+    localhost: {
+      url: node_url('localhost'),
+      accounts: accounts(),
+    },
+    mainnet: {
+      url: node_url('mainnet'),
+      accounts: accounts('mainnet'),
+    },
+    rinkeby: {
+      url: node_url('rinkeby'),
+      accounts: accounts('rinkeby'),
+    },
+    kovan: {
+      url: node_url('kovan'),
+      accounts: accounts('kovan'),
+    },
+    staging: {
+      url: node_url('kovan'),
+      accounts: accounts('kovan'),
+    },
+  },
+  paths: {
+    sources: 'src',
+  },
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 100,
+    enabled: process.env.REPORT_GAS ? true : false,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    maxMethodDiff: 10,
+  },
+  typechain: {
+    outDir: 'typechain',
+    target: 'ethers-v5',
+  },
+  mocha: {
+    timeout: 0,
+  },
+  external: process.env.HARDHAT_FORK
+    ? {
+        deployments: {
+          // process.env.HARDHAT_FORK will specify the network that the fork is made from.
+          // this line allow it to fetch the deployments from the network being forked from
+          hardhat: ['deployments/' + process.env.HARDHAT_FORK],
+        },
+      }
+    : undefined,
+};
+
+export default config;
